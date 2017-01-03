e8a2427902afaf99ec57904f9a3d64cc70e7c15e
davidrhmiller <davidrhmiller@users.noreply.github.com>
2017-01-03 12:48:10 -0500

    Adds a comment to the loop.
    Not really needed, but I like to talk.

--- dm/mini-projects/names.py
+++ dm/mini-projects/names.py
@@ -1,5 +1,7 @@
 # A project to greet someone by name
 
 response = raw_input("What's your name?\n--> ")
+
+# Print out a greeting 10 times.
 for i in range(1, 10):
     print "Hello, %s. Nice to meet you." % response
