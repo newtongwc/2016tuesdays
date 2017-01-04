d897af3722268b3ae9fd5439ac9b72ad42f71200
annazhang <anna_zhang@newtongwc.org>
2017-01-03 19:47:56 -0500

    creates new color talking program

---
+++ az/colors.py
@@ -0,0 +1,6 @@
+colors = ("pink", "purple", "turquoise", "mint", "black", "blush")
+
+i = 0
+while i < len(colors):
+    print "when i was %d, my favorite color was %s" % (i, colors[i])
+    i = i + 1
