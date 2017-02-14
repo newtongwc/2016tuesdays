93a1b978c50a887485ae6de5c67824d816ba100f
David Miller <david@newtongwc.org>
2017-02-13 21:10:13 -0500

    Adds the necessary boilerplate for AppEngine to run the oddjobs website.
    Also touches up some links in "End Page.html" to use relative paths instead of absolute ones.

--- oddjobs/oddjobs.py
+++ oddjobs/oddjobs.py
@@ -3,7 +3,7 @@ from flask import Flask
 # The parameter static_url_path tells Flask where to look for the "static" directory.
 # Requests for paths not explicitly listed with @app.route() directives below will be
 # interpreted as requests for files from the "static" directory.
-app = Flask('OddJobs', static_url_path='')
+app = Flask(__name__, static_url_path='')
 
 # Tell Flask to send the static file static/index.html if no path is given.
 @app.route("/")
@@ -11,4 +11,5 @@ def show_index():
     return app.send_static_file('index.html')
 
 
-app.run()
+if __name__ == "__main__":
+    app.run()
