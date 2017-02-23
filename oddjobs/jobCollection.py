022256cf696ce6ff42e6f4ba856128fb4d62ca7c
cornflower26 <ap666a@gmail.com>
2017-02-23 23:53:26 +0100

    Months

---
+++ oddjobs/jobCollection.py
@@ -0,0 +1,26 @@
+from Job import Job
+
+class JobCollection:
+    def __init__(self):
+        return
+
+    def add_job(self, Job):
+        return
+
+    def get_jobs(self):
+        """shows the user all of the jobs"""
+        return []
+
+    def get_job(self, Job_id):
+        """returns a specific job/selects it"""
+
+    def remove_job(self, Job_id):
+    """ removes the job (id) from the collection"""
+
+    def update_job(self, Job):
+    """ updates the job that is yours"""
+
+    def owners_jobs(self, name):
+        """returns all jobs under this name"""
+        return []
+
