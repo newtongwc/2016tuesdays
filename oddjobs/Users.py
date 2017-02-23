76d0b27fb5fbad9892d5f26b085b779ffc99d3c8
cornflower26 <ap666a@gmail.com>
2017-02-23 23:54:04 +0100

    Months

---
+++ oddjobs/Users.py
@@ -0,0 +1,8 @@
+class Users:
+
+    def __init__(self, username, password, email):
+        self.userid = None
+        self.username = username
+        self.password = password
+        self.email = email
+
