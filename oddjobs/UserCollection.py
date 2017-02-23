50e352079ce828561a91593aa4afa8c97f3102aa
cornflower26 <ap666a@gmail.com>
2017-02-23 23:53:55 +0100

    Months

---
+++ oddjobs/UserCollection.py
@@ -0,0 +1,22 @@
+from Users import Users
+
+class UserCollection:
+
+    def __init__(self):
+        return
+
+    def add_user(self, User):
+        return
+
+    def get_users(self):
+        """shows the user all of the jobs"""
+        return []
+
+    def get_users(self, Users_userid):
+        """returns a specific job/selects it"""
+
+    def remove_user(self, Users_userid):
+    """ removes the job (id) from the collection"""
+
+    def update_user(self, Users):
+    """ updates the job that is yours"""
