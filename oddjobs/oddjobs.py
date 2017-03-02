93a2ee54eb2a560573705fba952bf512ecbb3f0a
Ella <ella_fontenot@newtongwc.org>
2017-03-02 07:49:12 -0500

    saving

--- oddjobs/oddjobs.py
+++ oddjobs/oddjobs.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 # The parameter static_url_path tells Flask where to look for the "static" directory.
 # Requests for paths not explicitly listed with @app.route() directives below will be
@@ -6,9 +7,21 @@ from flask import Flask
 app = Flask(__name__, static_url_path='')
 
 # Tell Flask to point users to the home page if no path is given.
+
+
 @app.route("/")
 def show_home_page():
     return app.send_static_file('homepage.html')
+
+
+@app.route("/login")
+def show_login():
+    return render_template('LoginMock.html')
+
+
+@app.route("/signup")
+def show_signup():
+    return render_template('SignUpsMock.html')
 
 
 if __name__ == "__main__":
